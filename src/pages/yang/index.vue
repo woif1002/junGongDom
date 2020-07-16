816dc7fe39fb3fabc0bc37f327c94cff42a8c820
yangdongtao <651771296@qq.com>
2020-07-16 11:17:49 +0800

    更改

--- src/pages/yang/index.vue
+++ src/pages/yang/index.vue
@@ -64,7 +64,7 @@ export default {
     init() {
       const viewer = new Cesium.Viewer("cesiumContainer", {
         animation: false, //是否显示动画控件(左下方那个)
-        baseLayerPicker: false, //是否显示图层选择控件
+        baseLayerPicker: false, //是否显示图层选择控
         geocoder: false, //是否显示地名查找控件
         timeline: false, //是否显示时间线控件
         sceneModePicker: true, //是否显示投影方式控件
